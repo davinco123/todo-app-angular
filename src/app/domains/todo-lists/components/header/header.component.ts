4661fccd2cf1cb2084e50ddce90aff66cb946fbc
Minh Lai <nhatminhngo11@gmail.com>
2022-09-12 13:36:04 +0700

    update calendar to get date dynamically

--- src/app/domains/todo-lists/components/header/header.component.ts
+++ src/app/domains/todo-lists/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-lists-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Output() modeChange = new EventEmitter<string>();
+  today: number = new Date().getDate();
+  month: string = new Date().toLocaleString('default', { month: 'long' });
+
+  ngOnInit(): void {}
 
   toCompleted() {
     this.modeChange.emit('Completed');
